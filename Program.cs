8828a7809b48e27b3adff3faf9e28c32b3f7e88d
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-11-10 16:09:40 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programmingCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
