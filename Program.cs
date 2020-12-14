99150ae3a2a27dc47040c2ca04c87ecf5347f822
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-12-14 11:52:26 +0700

    lw15

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿/*
+EX1 - первое задание
+EX2 - второе по четвертое задание
+*/
+#define EX1
+
+using System;
+using System.Linq;
 
 namespace programmingCs
 {
@@ -9,7 +13,26 @@ namespace programmingCs
     {
         static void Main()
         {
+#if EX1
+            string[] month =
+            {
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            };
+            var n = Convert.ToInt32(Console.ReadLine());
+            var monthSelect = from i in month
+                where i.Length == n
+                select i;
+            foreach (var elm in monthSelect)
+                Console.WriteLine(elm);
 
+#endif
+#if EX2
+#endif
+#if EX3
+#endif
+#if EX4
+#endif
         }
     }
 
